e9a64b00209314676ca9a9960a1dd857ac03b42d
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-10-22 09:04:58 -0700

    ,

--- project01_number-guessing-game/main.js
+++ project01_number-guessing-game/main.js
@@ -1,1 +1,16 @@
-export {};
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "Enter your guess between 1 1o 10"
+    }
+]);
+let computerGuess = Math.round(Math.random() * 10);
+if (computerGuess === answers.userGuess) {
+    console.log(chalk.blueBright("Congratulation! You Won The Game"));
+}
+else {
+    console.log(chalk.redBright(`The number was ${computerGuess}, You lost! Better Luck Next Time`));
+}
